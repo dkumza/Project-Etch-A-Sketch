f42a9e3268a88f95e2218e6667e8343938dfa054
Darius Kumza <dariuskumza@gmail.com>
2023-01-11 22:29:09 +0200

    working example

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const grid_cont = document.querySelector('#grid-cont');
-
 // create n x n grid layout
+const grid_cont = document.querySelector('#grid-cont');
 function createGrid(n) {
   let grid_n = n * n;
   for (let i = 1; i <= grid_n; i++) {
     let grid_item = document.createElement('div');
     grid_item.classList.add('grid-item');
     grid_cont.appendChild(grid_item);
-    grid_cont.style.cssText = `grid-template-columns: repeat(${n}, auto);`;
+    grid_cont.style.cssText = `
+    grid-template-columns: repeat(${n}, auto);
+    grid-template-rows: repeat(${n}, auto)
+  `;
   }
 }
+createGrid(64);
 
-createGrid(16);
+// change background color of grid item on "hover"
+const grid_items = document.querySelectorAll('div.grid-item');
+grid_items.forEach((grid_items) =>
+  grid_items.addEventListener('mouseover', (e) => {
+    grid_items.style.cssText = `background-color: purple;`;
+  })
+);
